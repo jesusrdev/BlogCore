b064b9864a3ebb9a1302afd54da936289be6d9e1
jesus <rojasalarcone@gmail.com>
2024-06-21 15:24:08 -0500

    Agregar archivos de proyecto.

---
+++ BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ BlogCore.Models/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un nombre para la categoria")]
+        [Display(Name = "Nombre de Categoria")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Orden de Visualizacion")]
+        public int? Orden { get; set; }
+
+
+    }
+}
