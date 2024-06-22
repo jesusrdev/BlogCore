2d94162c01ae28ca26b305ebc17083b91fd931e7
jesus <rojasalarcone@gmail.com>
2024-06-22 08:31:05 -0500

    Crendo la vista Index de Categorias que usa ajax para recibir la lista en un json y los muestra con DataTables

---
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,38 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+
+        public CategoriasController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        #region Llamadas a la API
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+
+        #endregion
+    }
+}
