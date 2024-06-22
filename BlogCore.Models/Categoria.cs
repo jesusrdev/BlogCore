ad46a5cacc7a9ea7dc510f27bafcd81c54c622ac
jesus <rojasalarcone@gmail.com>
2024-06-22 12:30:47 -0500

    Probando las validaciones del lado del cliente y del lado del servidor

--- BlogCore.Models/Categoria.cs
+++ BlogCore.Models/Categoria.cs
@@ -17,6 +17,7 @@ namespace BlogCore.Models
         public string Nombre { get; set; }
 
         [Display(Name = "Orden de Visualizacion")]
+        [Range(1, 100, ErrorMessage = "El valor debe estar entre 1 y 100")]
         public int? Orden { get; set; }
 
 
